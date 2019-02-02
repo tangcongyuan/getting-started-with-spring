72951d7fe5cc019d71d5c39968d5611782f01387
Eric (Congyuan) Tang <tangcongyuan@gmail.com>
2019-02-02 15:55:58 -0800

    Initial commit

---
+++ src/main/java/com/erictang/gettingstartedwithspring/GettingStartedWithSpringApplication.java
@@ -0,0 +1,14 @@
+package com.erictang.gettingstartedwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GettingStartedWithSpringApplication.class, args);
+    }
+
+}
+
